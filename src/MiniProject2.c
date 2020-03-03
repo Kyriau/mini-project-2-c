272cf84cf8d96d08b0f38a2182661ca6425c18c2
Jeff <jeff.aj.thomson@gmail.com>
2020-03-02 20:39:31 -0800

    added thread and request structs

--- src/MiniProject2.c
+++ src/MiniProject2.c
@@ -2,17 +2,38 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int main() {
+typedef struct _request_t {
+	int id;
+	int length;
+} request_t;
 
+typedef struct _thread_t {
+	int id;
+	pthread_t thread;
+} thread_t;
+
+void *master(int n, int m) {
+
+}
+
+void *slave() {
+
+}
+
+int main(int argc, char **argv) {
+
+	// Get n and m from user
 	int n, m;
-
 	printf("How many consumer threads to you want? ");
 	scanf("%i", &n);
-
 	printf("How many seconds should the maximum request length be? ");
 	scanf("%i", &m);
 
-	printf("n=%i, m=%i\n", n, m);
+	// Generate thread ids;
+	thread_t threads[n + 1];
+	for(int i = 0; i < n + 1; i++) {
+		threads[i].id = i;
+	}
 
 	exit(0);
 
