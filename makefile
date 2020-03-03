9405f366554dd830ba6c34a48d88ce09d9e51948
Jeff <jeff.aj.thomson@gmail.com>
2020-03-03 14:11:42 -0800

    some stuffuff for pthreads added

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 MiniProject2: src/MiniProject2.c
-	gcc -o MiniProject2 src/MiniProject2.c
+	gcc -o MiniProject2 src/MiniProject2.c -pthread
--- src/MiniProject2.c
+++ src/MiniProject2.c
@@ -2,37 +2,45 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-typedef struct _request_t {
+typedef struct {
 	int id;
 	int length;
 } request_t;
 
-typedef struct _thread_t {
+typedef struct {
 	int id;
 	pthread_t thread;
 } thread_t;
 
-void *master(int n, int m) {
+typedef struct {
+	int n;
+	int m;
+} master_args_t;
 
+void *master(void *arg) {
+	
 }
 
-void *slave() {
+void *slave(void *arg) {
 
 }
 
 int main(int argc, char **argv) {
 
 	// Get n and m from user
-	int n, m;
+	master_args_t nm;
 	printf("How many consumer threads to you want? ");
-	scanf("%i", &n);
+	scanf("%i", &nm.n);
 	printf("How many seconds should the maximum request length be? ");
-	scanf("%i", &m);
+	scanf("%i", &nm.m);
 
-	// Generate thread ids;
+	// Generate threads;
 	thread_t threads[n + 1];
-	for(int i = 0; i < n + 1; i++) {
+	threads[0].id = 0;
+	pthread_create(&threads[0].thread, NULL, master, (void *) &nm);
+	for(int i = 1; i < n + 1; i++) {
 		threads[i].id = i;
+		pthread_create(&threads[i].thread, NULL, slave, NULL);
 	}
 
 	exit(0);
